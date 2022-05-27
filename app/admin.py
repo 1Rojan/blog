a7d11e713c1fcffd2733c39dcf2385df8fca9dd3
1Rojan <rznsnr11@gmail.com>
2022-05-27 13:03:22 +0545

    post list and filter

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('title', 'slug', 'status', 'created_on')
+  list_filter = ('status',)
+  search_fields = ['title', 'content']
+admin.site.register(Post, PostAdmin)
